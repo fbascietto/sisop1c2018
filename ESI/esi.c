944952e769a27211f30a34dd926eafb63dbaf9c2
Gonzalo Lepetic <gonzalolepetic@hotmail.com>
2018-04-14 19:27:30 -0300

    Inicializacion basica de esi (para admitir parametros)

--- ESI/esi.c
+++ ESI/esi.c
@@ -1,3 +1,15 @@
-int main(){
-	return 0;
+#include "esi.h"
+
+void main(int args, char* argv[]) {
+
+	/* Descomentar mas tarde...
+	  if(args != 2){
+
+		log_error(error_log, "Cantidad de parametros incorrectos.");
+		exit(0);
+
+	}*/
+
+	ruta_script_ejecuciones = argv[1];
+
 }
---
+++ ESI/esi.h
@@ -0,0 +1,13 @@
+/*
+ * esi.h
+ *
+ *  Created on: 14 abr. 2018
+ *      Author: utnso
+ */
+
+#ifndef ESI_H_
+#define ESI_H_
+
+char* ruta_script_ejecuciones;
+
+#endif /* ESI_H_ */
