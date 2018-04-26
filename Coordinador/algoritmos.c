dd70d90a746fe76736aad738e575dc95219d3c90
utnso <agustinlabarc@hotmail.com>
2018-04-26 14:34:28 -0300

    agrego equitative load

---
+++ Coordinador/algoritmos.c
@@ -0,0 +1,22 @@
+/*
+ * algoritmos.c
+ *
+ *  Created on: Apr 26, 2018
+ *      Author: utnso
+ */
+#include "coordinador.h"
+
+
+void equitativeLoad(t_solicitud una_solicitud){
+	t_instancia* instancia = list_get(instancias, posicion_puntero);
+	despachar_solicitud(instancia, una_solicitud);
+	posicion_puntero = siguiente(posicion_puntero, list_size(instancias));
+}
+
+int siguiente(int posicion_puntero, int tamanio){
+	if((++posicion_puntero) < tamanio){
+		return posicion_puntero;
+	} else{
+		return 0;
+	}
+}
--- Coordinador/coordinador.h
+++ Coordinador/coordinador.h
@@ -8,6 +8,7 @@
 #include <commons/log.h>
 #include <commons/config.h>
 #include <commons/string.h>
+#include <commons/collections/list.h>
 #include <readline/readline.h> // Para usar readline
 #include "../Recursos/estructuras.h"
 #include "../Recursos/protocolo.h"
@@ -19,7 +20,24 @@ int cantidad_Entradas;
 int tamanio_Entrada;
 int retardo;
 
+t_list* instancias;
+//para equitative load, posicion del puntero en la lista
+int posicion_puntero;
+
+
+typedef struct {
+	int id;
+} t_instancia;
+
+typedef struct {
+	int id;
+	char* key;
+	char* clave;
+	char* accion;
+} t_solicitud;
+
 void *esperarConexiones(void *args);
 void cargarConfiguracion();
+void despachar_solicitud(t_instancia unaInstancia, t_solicitud una_solicitud);
 
 #endif /* COORDINADOR_H_ */
