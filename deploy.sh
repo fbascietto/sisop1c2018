b564f2bc92636de55b3263af662b2781f8927568
fbascietto <fran.ba22@gmail.com>
2018-07-14 05:17:41 -0300

    1er deploy DR

---
+++ deploy.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+# Deploy Destino Rusia
+
+cd /home/utnso/
+
+git clone https://github.com/sisoputnfrba/so-commons-library.git
+git clone https://github.com/sisoputnfrba/parsi
+git clone https://github.com/sisoputnfrba/Pruebas-ESI
+
+cd /home/utnso/so-commons-library/
+sudo make install
+
+cd /home/utnso/parsi/src
+sudo make install
+
+cd /home/utnso/tp-2018-1c-Destino-Rusia/Biblioteca/makefiles
+sudo make
+
+sudo cp libBiblioteca.so /usr/lib
+
+cd /home/utnso/tp-2018-1c-Destino-Rusia/Coordinador/makefiles
+sudo make
+cd /home/utnso/tp-2018-1c-Destino-Rusia/Instancia/makefiles
+sudo make
+cd /home/utnso/tp-2018-1c-Destino-Rusia/Planificador/makefiles
+sudo make
+cd /home/utnso/tp-2018-1c-Destino-Rusia/ESI/makefiles
+sudo make
+
+cd /home/utnso/tp-2018-1c-Destino-Rusia/Instancia/
+
+cp -r makefiles instancia1
+cp -r makefiles instancia2
+cp -r makefiles instancia3 
